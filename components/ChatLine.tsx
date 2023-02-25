521273a3cdf36baaea5486da7647f0c22a479a7a
noahsploiter <noahsploit@gmail.com>
2023-02-25 15:30:13 -0800

    'c'

--- components/ChatLine.tsx
+++ components/ChatLine.tsx
@@ -1,13 +1,13 @@
-import clsx from 'clsx'
-import Balancer from 'react-wrap-balancer'
+import clsx from "clsx";
+import Balancer from "react-wrap-balancer";
 
 // wrap Balancer to remove type errors :( - @TODO - fix this ugly hack
-const BalancerWrapper = (props: any) => <Balancer {...props} />
+const BalancerWrapper = (props: any) => <Balancer {...props} />;
 
 export type Message = {
-  who: 'bot' | 'user' | undefined
-  message?: string
-}
+  who: "bot" | "user" | undefined;
+  message?: string;
+};
 
 // loading placeholder animation for the chat line
 export const LoadingChatLine = () => (
@@ -29,27 +29,27 @@ export const LoadingChatLine = () => (
       </div>
     </div>
   </div>
-)
+);
 
 // util helper to convert new lines to <br /> tags
 const convertNewLines = (text: string) =>
-  text.split('\n').map((line, i) => (
+  text.split("\n").map((line, i) => (
     <span key={i}>
       {line}
       <br />
     </span>
-  ))
+  ));
 
-export function ChatLine({ who = 'bot', message }: Message) {
+export function ChatLine({ who = "bot", message }: Message) {
   if (!message) {
-    return null
+    return null;
   }
-  const formatteMessage = convertNewLines(message)
+  const formatteMessage = convertNewLines(message);
 
   return (
     <div
       className={
-        who != 'bot' ? 'float-right clear-both' : 'float-left clear-both'
+        who != "bot" ? "float-right clear-both" : "float-left clear-both"
       }
     >
       <BalancerWrapper>
@@ -58,13 +58,13 @@ export function ChatLine({ who = 'bot', message }: Message) {
             <div className="flex-1 gap-4">
               <p className="font-large text-xxl text-gray-900">
                 <a href="#" className="hover:underline">
-                  {who == 'bot' ? 'AI' : 'You'}
+                  {who == "bot" ? "AI" : "You"}
                 </a>
               </p>
               <p
                 className={clsx(
-                  'text ',
-                  who == 'bot' ? 'font-semibold font- ' : 'text-gray-400'
+                  "text ",
+                  who == "bot" ? "font-semibold font- " : "text-gray-400"
                 )}
               >
                 {formatteMessage}
@@ -74,5 +74,5 @@ export function ChatLine({ who = 'bot', message }: Message) {
         </div>
       </BalancerWrapper>
     </div>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,26 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
-import { Chat } from '../components/Chat'
+import { Chat } from "../components/Chat";
 
 function Home() {
   return (
-    <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
-        <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
-        </Text>
+    <div className="flex flex-co">
+      <section className="w-full">
+        <div className="text-2xl font-extrabold">OpenAI GPT-3</div>
+        <div className=" items-center">
+          <img
+            className="mx-auto rounded-full w-[100px] h-[100px] mt-[50px]"
+            src="https://cdn.vox-cdn.com/thumbor/VUn58Srehbu5brDicV6QgNp8SM0=/0x0:1820x1213/1400x1400/filters:focal(910x607:911x608)/cdn.vox-cdn.com/uploads/chorus_asset/file/24247717/lp_logo_3.0.jpg"
+            alt=""
+          />
+        </div>
       </section>
-
-      <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+      <section className="absolute inset-x-0 bottom-[20px]">
+        <div>Noah-GptBot:</div>
         <div className="lg:w-2/3">
           <Chat />
         </div>
       </section>
-    </Page>
-  )
+    </div>
+  );
 }
 
-Home.Layout = Layout
-
-export default Home
+export default Home;
