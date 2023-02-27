1cde08a4947b20a04a5feb8b931642690a2c29aa
noahsploiter <noahsploit@gmail.com>
2023-02-27 01:32:03 -0800

    'c'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,20 @@
+import { Layout, Text, Page } from "@vercel/examples-ui";
 import { Chat } from "../components/Chat";
 
 function Home() {
   return (
-    <div className="">
-      <div className="flex flex-co">
-        <section className="w-full">
-          <div className="text-2xl font-extrabold pl-[30px]">OpenAI GPT-3</div>
-          <div className=" items-center">
-            <img
-              className="mx-auto rounded-full w-[100px] h-[100px] mt-[50px]"
-              src="https://cdn.vox-cdn.com/thumbor/VUn58Srehbu5brDicV6QgNp8SM0=/0x0:1820x1213/1400x1400/filters:focal(910x607:911x608)/cdn.vox-cdn.com/uploads/chorus_asset/file/24247717/lp_logo_3.0.jpg"
-              alt=""
-            />
-          </div>
-        </section>
-        <section className="absolute inset-x-0 bottom-[20px] ">
-          <div className="w-fit top-0 sticky pl-[30px] bg-white">
-            Noah-GptBot:
-          </div>
-          <div className="lg:w-2/3">
-            <Chat />
-          </div>
-        </section>
-      </div>
-    </div>
+    <Page className="flex flex-col gap-12">
+      <section className="flex flex-col gap-6">
+        <Text variant="h1">OpenAI GPT-3</Text>
+      </section>
+
+      <section className="flex flex-col gap-3">
+        <Text variant="h2">Noah-GPT-bot</Text>
+        <div className="lg:w-2/3">
+          <Chat />
+        </div>
+      </section>
+    </Page>
   );
 }
 
